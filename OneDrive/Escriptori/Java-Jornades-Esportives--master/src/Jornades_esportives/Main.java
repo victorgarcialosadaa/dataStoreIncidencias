d32decc326a077dd8ce6274756368085f2284ab1
Victor Garcia <vgarcialosada@gmail.com>
2019-02-01 15:34:04 +0100

    branch victor nueva

--- OneDrive/Escriptori/Java-Jornades-Esportives--master/src/Jornades_esportives/Main.java
+++ OneDrive/Escriptori/Java-Jornades-Esportives--master/src/Jornades_esportives/Main.java
@@ -49,5 +49,5 @@ public class Main {
         }
 
     }public static void victorBranch(){
-            System.out.println("Cambios hechos por Victor 2");}
+            System.out.println("Cambios hechos por Victor 2ssdzb");}
 }
